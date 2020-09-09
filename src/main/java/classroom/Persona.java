9b912015af90ae990736da99847252af8d4c08a5
CarlosMarioCA <Camchavezag@unal.edu.co>
2020-09-08 22:26:52 -0500

    5

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -4,7 +4,8 @@ public class Persona {
 
     long cedula;
     String nombre;
-    static int totalPersonas;
+    private static int totalPersonas;
+    private static int noIdentificados = 0;
 
     static {
         totalPersonas = 0;
@@ -31,8 +32,9 @@ public class Persona {
     Persona(String nombre) {
         //Hay que colocar modificador aqui para que no se repita la cedula.
         //O Eliminar porque no se deberia poder ingresar una persona sin documento.
-        this.cedula = totalPersonas;
+        this.cedula = getNoIdentificados();
         this.nombre = nombre;
+        noIdentificados++;
         totalPersonas++;
     }
 
@@ -47,4 +49,22 @@ public class Persona {
     void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public static int getTotalPersonas() {
+        return totalPersonas;
+    }
+
+    public static void setTotalPersonas(int totalPersonas) {
+        Persona.totalPersonas = totalPersonas;
+    }
+
+    public static int getNoIdentificados() {
+        return noIdentificados;
+    }
+
+    public static void setNoIdentificados(int noIdentificados) {
+        Persona.noIdentificados = noIdentificados;
+    }
+    
+    
 }
